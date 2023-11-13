e8159c4175442176a0629d0483b83a5ee6ebbf7a
ERIC ESPINOSA <86639913+a1020427643@users.noreply.github.com>
2023-11-13 11:08:13 -0500

    Change destiny url
    do this change for redirect the navigator

---
+++ src/Buscador.java
@@ -0,0 +1,19 @@
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.*;
+
+public class Buscador {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.Chrome.driver", "C:\\Users\\eespinosam\\Documents\\drivers\\chromedriver\\chromedriver.exe");
+		WebDriver driver_chrome = new ChromeDriver();
+		driver_chrome.get("https://admin-test.cuida.pe");
+		
+		
+		
+
+	}
+
+}
